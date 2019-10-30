2e37ebd0ce5c2bcd59654782e1f9ab7dbcb6da9c
Mostafa Mahmoud <Mostafafci.2008@gmail.com>
2019-10-30 19:49:32 +0200

    Social Package Push

---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'github' => [
+        'client_id' => env('2795e89c0278649441c0'),
+        'client_secret' => env('6494ffc84a52a4869c2c4cde536b95541c46dc4d'),
+        'redirect' => 'https://devmostafa.github.io/social.github.io/',
+    ],
+
+];
